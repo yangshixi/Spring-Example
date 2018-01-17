84e18bcf4cbbc50c4fec9544e7acf56280b2f3b9
yangshixi <yangshixi01@163.com>
2018-01-17 11:51:25 +0800

    `

--- Spring052/src/main/java/com/zhangguo/Spring052/aop03/Test.java
+++ Spring052/src/main/java/com/zhangguo/Spring052/aop03/Test.java
@@ -4,16 +4,15 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Test {
-
+	private static ApplicationContext ctx;
 	public static void main(String[] args) {
-		ApplicationContext ctx = new ClassPathXmlApplicationContext("aop03.xml");
+		ctx = new ClassPathXmlApplicationContext("aop03.xml");
 		IMath math = ctx.getBean("math", Math.class);
 		int n1 = 100, n2 = 5;
 		math.add(n1, n2);
 		math.sub(n1, n2);
 		math.mut(n1, n2);
 		math.div(n1, n2);
-		
 		StrUtil strUtil=ctx.getBean("strUtil",StrUtil.class);
 		strUtil.show();
 	}
